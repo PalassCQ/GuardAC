e643e2d4d04fea502b658c90782ba0af40ca8de1
PalassCQ <ablacktopbg@gmail.com>
2026-07-21 13:42:22 +0500

    Update CheckRegistry.kt

--- src/main/kotlin/dev/guardac/checks/CheckRegistry.kt
+++ src/main/kotlin/dev/guardac/checks/CheckRegistry.kt
@@ -14,6 +14,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * This file contains code derived from:
+ *   - SlothAC (© 2025 KaelusMC, https://github.com/KaelusMC/SlothAC)
+ * All derived code is licensed under GPL-3.0.
  */
 
 package dev.guardac.checks
