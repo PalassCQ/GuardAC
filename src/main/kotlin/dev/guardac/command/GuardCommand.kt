596cd9738d98c6303245ccf36cef86a7ee2d087d
PalassCQ <ablacktopbg@gmail.com>
2026-07-21 13:58:20 +0500

    Update GuardCommand.kt

--- src/main/kotlin/dev/guardac/command/GuardCommand.kt
+++ src/main/kotlin/dev/guardac/command/GuardCommand.kt
@@ -16,6 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * This file contains code derived from:
  *   - SlothAC (© 2025 KaelusMC, https://github.com/KaelusMC/SlothAC)
+ *   - Grim (© 2025 GrimAnticheat, https://github.com/GrimAnticheat/Grim)
  * All derived code is licensed under GPL-3.0.
  */
 
